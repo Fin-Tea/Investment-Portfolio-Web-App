9146379c7725afa3a62d8dd7acea9fc12c000c3b
Jabari Holloway <jabari@jabariholloway.com>
2024-01-09 15:19:24 -0600

    Add journal date format function

--- client/date-utils.js
+++ client/date-utils.js
@@ -18,6 +18,20 @@ export default function formatDate(date) {
   return `${months[d.getMonth()]} ${d.getDate()}, ${d.getFullYear()}`;
 }
 
+export function formatJournalDate(date) {
+  const d = new Date(date);
+  let h = d.getHours();
+  let m = d.getMinutes();
+
+  const amPM = h >= 12 ? "PM" : "AM";
+  h = h % 12;
+  if (m < 10) {
+    m = `0${m}`;
+  }
+
+  return `${months[d.getMonth()]}. ${d.getDate()}, ${d.getFullYear()} @ ${h}:${m} ${amPM}`;
+}
+
 export function formatDateShort(date) {
   const d = new Date(date);
   return `${months[d.getMonth()]} ${d.getDate()}`;
