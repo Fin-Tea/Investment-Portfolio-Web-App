34c234fd06e998cef7b852c38672f17ac9e577fb
Jabari Holloway <jabari@jabariholloway.com>
2024-01-09 15:17:43 -0600

    Add Chakra UI lib

--- client/pages/_app.js
+++ client/pages/_app.js
@@ -1,12 +1,17 @@
 import RouteGuard from "../components/route-guard";
+import { ChakraProvider } from '@chakra-ui/react'
 import "../styles/globals.css";
 import "bootstrap/dist/css/bootstrap.min.css";
-import "react-loader-spinner/dist/loader/css/react-spinner-loader.css";
+import "react-datepicker/dist/react-datepicker.css";
+//import "react-loader-spinner/dist/loader/css/react-spinner-loader.css";
+
 
 function MyApp({ Component, pageProps }) {
   return (
     <RouteGuard>
+      <ChakraProvider>
       <Component {...pageProps} />
+      </ChakraProvider>
     </RouteGuard>
   );
 }
