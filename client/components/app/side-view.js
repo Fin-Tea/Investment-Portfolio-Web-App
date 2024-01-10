7634b6d4191542365a9110dfe5aa039e12a02e9a
Jabari Holloway <jabari@jabariholloway.com>
2024-01-09 18:29:31 -0600

    Show side-view name when collapsed

--- client/components/app/side-view.js
+++ client/components/app/side-view.js
@@ -12,12 +12,13 @@ export default function SideView({className, header, children }) {
     const width = isExpanded ? "w-60" : "w-12";
 
   return (
-    <div className={`border-gray-300 border-t border-r border-b p-2 w-60 h-full ${width} ${className}`} >
+    <div className={`border-gray-300 border-t border-r border-b p-2 h-full ${width} ${className}`} >
         <div className="flex">
         {isExpanded && (<h3 className="h-7 text-lg w-full">{header}</h3>)}
         <FontAwesomeIcon className="cursor-pointer" onClick={toggleExpanded} icon={isExpanded ? faAnglesLeft : faAnglesRight} height={32} />
         </div>
         {isExpanded && (<div className="h-full flex flex-col">{children}</div>)}
+        {!isExpanded && (<div className="h-full flex flex-col ml-2 mt-2">{header.split('').map((char) => <p className={ char === ' ' ? 'mb-2' : 'mb-0'}>{char}</p>)}</div>)}
         
     </div>
   );
