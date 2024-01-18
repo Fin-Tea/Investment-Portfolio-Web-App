748b0e5c106dd65ddfe18fee3992142c15aa4d52
Jabari Holloway <jabari@jabariholloway.com>
2024-01-17 22:46:20 -0600

    Add placeholder to SearchBox component

--- client/components/app/search-box.js
+++ client/components/app/search-box.js
@@ -7,11 +7,11 @@ import { faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons'
 
 
 
-export default function SearchBox({ className, onClick }) {
+export default function SearchBox({ className, placeholder, onClick }) {
     const [text, setText] = useState("");
     
     return (<div className={`flex ${className}`}>
-        <input placeholder="Search" className="border-gray-300 border-x border-y px-2" onChange={(e) => setText(e.target.value)} />
+        <input placeholder={placeholder || "Search"} className="border-gray-300 border-x border-y px-2" onChange={(e) => setText(e.target.value)} />
         <FontAwesomeIcon className="ml-2 cursor-pointer" icon={faMagnifyingGlass} height={32} onClick={() => onClick && onClick(text)} />
     </div>);
 
