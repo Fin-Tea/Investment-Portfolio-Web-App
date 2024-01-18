eae93fd19e34ff9b97e3ec0f97597e04c1780ad4
Jabari Holloway <jabari@jabariholloway.com>
2024-01-17 22:45:17 -0600

    Add site nav links

--- client/components/app/layout.js
+++ client/components/app/layout.js
@@ -1,7 +1,18 @@
+import { useRouter } from "next/router";
 import Head from "next/head";
 import AccountMenu from "./account-menu";
+import LayoutLink from "./layout-link";
+
+const ROUTES = {
+  JOURNAL: "/app/trading-journal",
+  TRADES: "/app/trades",
+  INSIGHTS: "/app/performance-insights",
+};
 
 export default function Layout({ children }) {
+  const router = useRouter();
+
+  const route = router.pathname;
 
   return (
     <div>
@@ -11,15 +22,24 @@ export default function Layout({ children }) {
           name="description"
           content="Easy Trading Journaling &amp; Performance Insights"
         />
-        <meta name="og:title" content="The Fin Tea" />
+        <meta name="og:title" content="Fin.Tea" />
       </Head>
       <header className="h-12 bg-purple-800 flex justify-between items-center">
-        <h1 className="pt-2 ml-4 text-lg text-white">The Fin Tea</h1>
+        <h1 className="pt-2 ml-4 text-lg text-white">Fin Tea</h1>
+        <LayoutLink href={ROUTES.JOURNAL}>
+            Trading Journal
+        </LayoutLink>
+        <LayoutLink href={ROUTES.TRADES}>
+            Trades
+        </LayoutLink>
+        <LayoutLink href={ROUTES.INSIGHTS}>
+        Performance Insights
+        </LayoutLink>
         <div className="mr-4">
-        <AccountMenu />
+          <AccountMenu />
         </div>
       </header>
-        <main className="bg-zinc-100 h-screen">{children}</main>
+      <main className="bg-zinc-100 h-screen">{children}</main>
     </div>
   );
 }
