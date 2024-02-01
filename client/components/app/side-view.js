e5644808a9f7d5d5e2332228355c6b68c56e62d7
Jabari Holloway <jabari@jabariholloway.com>
2024-02-01 14:20:39 -0600

    Make SideView wider when expanded to show text

--- client/components/app/side-view.js
+++ client/components/app/side-view.js
@@ -2,20 +2,22 @@ import React, { useState } from "react";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faAnglesLeft, faAnglesRight } from '@fortawesome/free-solid-svg-icons'
 
-export default function SideView({className, header, children }) {
+export default function SideView({className, header, onToggle, children }) {
     const [isExpanded, setExpanded] = useState(true);
 
     function toggleExpanded() {
+        onToggle && onToggle(!isExpanded);
         setExpanded(!isExpanded);
+ 
     }
 
-    const width = isExpanded ? "w-60" : "w-12";
+    const width = isExpanded ? "w-72" : "w-12";
 
   return (
     <div className={`border-gray-300 border-t border-r border-b p-2 h-full ${width} ${className}`} >
         <div className="flex">
-        {isExpanded && (<h3 className="h-7 text-lg w-full">{header}</h3>)}
-        <FontAwesomeIcon className="cursor-pointer" onClick={toggleExpanded} icon={isExpanded ? faAnglesLeft : faAnglesRight} height={32} />
+        {isExpanded && (<h3 className="text-lg w-full">{header}</h3>)}
+        <FontAwesomeIcon className="cursor-pointer h-7" onClick={toggleExpanded} icon={isExpanded ? faAnglesLeft : faAnglesRight} height={32} />
         </div>
         {isExpanded && (<div className="h-full flex flex-col">{children}</div>)}
         {!isExpanded && (<div className="h-full flex flex-col ml-2 mt-2">{header.split('').map((char) => <p className={ char === ' ' ? 'mb-2' : 'mb-0'}>{char}</p>)}</div>)}
