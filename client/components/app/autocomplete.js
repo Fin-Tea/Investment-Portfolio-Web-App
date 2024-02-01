5fb6ee463397e1422ea89887d59d40c7a827d4e1
Jabari Holloway <jabari@jabariholloway.com>
2024-02-01 14:16:54 -0600

    Fix autocomplete

--- client/components/app/autocomplete.js
+++ client/components/app/autocomplete.js
@@ -1,46 +1,33 @@
-import React, {useState } from "react"
-import { ReactSearchAutocomplete } from "react-search-autocomplete";
+import React, { useState } from "react";
+import CreatableSelect from "react-select/creatable";
 import Tooltip from "./tooltip";
 
 export default function Autocomplete({
-    className,
+  className,
   placeholder,
-  onSearch,
   onSelect,
   items,
   tooltip,
-  value
+  value,
 }) {
-    const [isFocused, setIsFocused] = useState(false);
+  const [options, setOptions] = useState(items);
 
-    const handleOnFocus = () => {
-        console.log('Focused')
-        setIsFocused(true);
-      }
 
-  const handleBlur = () => {
-    setIsFocused(false);
-  }
-
-  return (<div className={`flex w-full items-center ${className}`}>
-    <div className="w-full" onBlur={handleBlur}>
-    <ReactSearchAutocomplete
-    className="w-full autocomplete"
-      showIcon={false}
-      styling={{ boxShadow: "none", borderRadius: "7px", zIndex: isFocused ? 10: 0 }}
-      placeholder={placeholder}
-      onFocus={handleOnFocus}
-      showNoResults={false}
-      onSearch={onSearch}
-      onSelect={onSelect}
-      items={items}
-      fuseOptions={{ keys: ["label"] }}
-      resultStringKeyName="label"
-      onBlur
-      inputSearchString={value || ""}
-    />
-    </div>
-    {tooltip && <Tooltip text={tooltip} />}
+  return (
+    <div className={`flex w-full items-center ${className}`}>
+      <div className="w-full">
+        <CreatableSelect
+          className="w-full"
+          placeholder={placeholder || ""}
+          options={options}
+          value={value}
+          onChange={onSelect}
+          isClearable
+          isSearchable
+          onCreateOption={(label) => {const item = {label}; setOptions([item, ...options]); onSelect && onSelect(item); }}
+        />
+      </div>
+      {tooltip && <Tooltip text={tooltip} />}
     </div>
   );
 }
