1466aa3627f1976963a38da47d2f600b64e30aff
Jabari Holloway <jabari@jabariholloway.com>
2024-02-01 14:19:22 -0600

    Add hover background to JournalEntry component

--- client/components/app/journal-entry.js
+++ client/components/app/journal-entry.js
@@ -1,8 +1,16 @@
+import React, { useState } from "react";
+
 export default function JournalEntry({ id, tag, date, entryText, symbol, onClick}) {
+    const [hover, setHover] = useState(false);
+
+    function toggleHover() {
+        setHover(!hover);
+    }
 
     const cursor = onClick ? "cursor-pointer" : "cursor-default";
+    const background = hover ? "bg-green-400" : "bg-transparent";
 
-    return (<div className={`flex flex-col w-full ${cursor}`} onClick={() => onClick && onClick(id)}>
+    return (<div className={`flex flex-col w-full p-1 ${cursor} ${background}`} onClick={() => onClick && onClick(id)} onMouseEnter={toggleHover} onMouseLeave={toggleHover}>
         <div className="flex flex-row justify-between font-bold">
             <span>{tag}</span>
             {symbol && <span>{symbol}</span>}
