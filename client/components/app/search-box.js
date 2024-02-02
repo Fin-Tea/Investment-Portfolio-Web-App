8fd6b7f94e6cf026b2220a948176271493987327
Jabari Holloway <jabari@jabariholloway.com>
2024-02-02 13:28:52 -0600

    Update SearchBox to search on type

--- client/components/app/search-box.js
+++ client/components/app/search-box.js
@@ -1,18 +1,11 @@
-import React, { useState } from "react";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons'
 
-
-
-
-
-
-export default function SearchBox({ className, placeholder, onClick }) {
-    const [text, setText] = useState("");
+export default function SearchBox({ className, placeholder, onClick, onSearch }) {
     
     return (<div className={`flex ${className}`}>
-        <input placeholder={placeholder || "Search"} className="border-gray-300 border-x border-y px-2" onChange={(e) => setText(e.target.value)} />
-        <FontAwesomeIcon className="ml-2 cursor-pointer" icon={faMagnifyingGlass} height={32} onClick={() => onClick && onClick(text)} />
+        <input placeholder={placeholder || "Search"} className="border-gray-300 border-x border-y px-2 w-full" onChange={(e) => onSearch && onSearch(e.target.value)} />
+        <FontAwesomeIcon className="ml-2" icon={faMagnifyingGlass} height={32} onClick={() => onClick && onClick(text)} />
     </div>);
 
 
