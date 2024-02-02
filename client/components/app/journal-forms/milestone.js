d447b38a1a21bbbb7251e4da3d3726d216df5fe3
Jabari Holloway <jabari@jabariholloway.com>
2024-02-02 13:31:14 -0600

    Add schema validation, react-hook-form, and wiring to Milestone form

--- client/components/app/journal-forms/milestone.js
+++ client/components/app/journal-forms/milestone.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import { useForm } from "react-hook-form";
 import { yupResolver } from "@hookform/resolvers/yup";
 import * as Yup from "yup";
 import DatePicker from "react-datepicker";
@@ -6,62 +6,117 @@ import BaseForm from "./base-form";
 import Pill from "../pill";
 import Tooltip from "../tooltip";
 
-
 export default function Milestone({ data, onSubmit, onDelete }) {
-    const validationSchema = Yup.object().shape({
-        growthTypeId: Yup.number()
-          .transform((value) => (Number.isNaN(value) ? null : value))
-          .required("Type required"),
-          milestoneText: Yup.string().required("Milestone is required"),
-      });
+  const validationSchema = Yup.object().shape({
+    growthTypeId: Yup.number()
+      .transform((value) => (Number.isNaN(value) ? null : value))
+      .required("Type required"),
+    milestoneText: Yup.string().required("Milestone is required"),
+  });
 
-      const formOptions = {
-        defaultValues: {
-          growthTypeId: data?.milestone.growthTypeId,
-          reachedOn: data?.milestone.reachedOn,
-          milestoneText: data?.milestone.milestoneText,
-        },
-        resolver: yupResolver(validationSchema),
-      };
+  const formOptions = {
+    defaultValues: {
+      growthTypeId: data?.milestone.growthTypeId,
+      reachedOn: data?.milestone.reachedOn ? new Date(data?.milestone.reachedOn) : null,
+      milestoneText: data?.milestone.milestoneText,
+    },
+    resolver: yupResolver(validationSchema),
+  };
 
+  const { register, handleSubmit, formState, setValue, watch } =
+    useForm(formOptions);
+  const { errors } = formState;
 
-  const [growthTypeId, setGrowthTypeIdId] = useState(1);
-  const [achievedOn, setAchievedOn] = useState(null);
-  const [milestone, setMilestone] = useState("");
+  const growthTypeId = watch("growthTypeId");
+  const reachedOn = watch("reachedOn");
 
-
-  function handlePillClick({id}) {
-    setGrowthTypeIdId(id);
+  function handlePillClick({ id }) {
+    setValue("growthTypeId", id);
   }
 
   return (
-    <BaseForm header="Milestone">
+    <BaseForm
+      header="Milestone"
+      edit={!!data}
+      onSave={handleSubmit(onSubmit)}
+      onDelete={onDelete}
+    >
       <div>
         <label className="text-sm ml">Type*</label>
         <div className="flex">
-            <Pill className="mr-2" key={1} id={1} controlled onClick={handlePillClick} isActive={growthTypeId === 1} >Earnings</Pill>
-            <Pill className="mr-2" key={2} id={2} controlled onClick={handlePillClick} isActive={growthTypeId === 2} >Habits</Pill>
-            <Pill className="mr-2" key={3} id={3} controlled onClick={handlePillClick} isActive={growthTypeId === 3} >Skillz</Pill>
-            <Pill className="mr-2" key={4} id={4} controlled onClick={handlePillClick} isActive={growthTypeId === 4} >Knowledge</Pill>
+          <Pill
+            className="mr-2"
+            key={1}
+            id={1}
+            controlled
+            onClick={handlePillClick}
+            isActive={growthTypeId === 1}
+          >
+            Earnings
+          </Pill>
+          <Pill
+            className="mr-2"
+            key={2}
+            id={2}
+            controlled
+            onClick={handlePillClick}
+            isActive={growthTypeId === 2}
+          >
+            Habits
+          </Pill>
+          <Pill
+            className="mr-2"
+            key={3}
+            id={3}
+            controlled
+            onClick={handlePillClick}
+            isActive={growthTypeId === 3}
+          >
+            Skillz
+          </Pill>
+          <Pill
+            className="mr-2"
+            key={4}
+            id={4}
+            controlled
+            onClick={handlePillClick}
+            isActive={growthTypeId === 4}
+          >
+            Knowledge
+          </Pill>
+        </div>
+        <div className="text-red-600 text-xs">
+          {errors.growthTypeId?.message}
         </div>
       </div>
       <div className="mt-4">
         <label className="text-sm ml">Date Reached</label>
         <div className="flex items-center">
-        <DatePicker className="border rounded-md text-sm p-2" selected={achievedOn} onChange={(date) => setAchievedOn(date)} />
-        <Tooltip text="(Keep blank until completed)" />
+          <DatePicker
+            className="border rounded-md text-sm p-2"
+            selected={reachedOn}
+            onChange={(date) => {
+              const now = new Date();
+              date.setHours(now.getHours());
+              date.setMinutes(now.getMinutes());
+              setValue("reachedOn", date);
+            }}
+          />
+          <Tooltip text="(Keep blank until completed)" />
         </div>
       </div>
-
 
       <div className="mt-4">
         <label className="text-sm ml">Milestone*</label>
         <div className="flex items-top">
-          <textarea className="border w-full rounded-md p-2" value={milestone} onChange={(e) => setMilestone(e.target.value)} />
+          <textarea
+            className="border w-full rounded-md p-2"
+            {...register("milestoneText")}
+          />
           <Tooltip text="Celebrate and keep track of achievements throughout your trading/investing journey!" />
         </div>
+        <div className="text-red-600 text-xs">{errors.reachedOn?.message}</div>
       </div>
-
     </BaseForm>
   );
 }
