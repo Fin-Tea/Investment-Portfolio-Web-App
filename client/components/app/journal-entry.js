8df300a37dbf8e8d70e33659535f1716386c82a2
Jabari Holloway <jabari@jabariholloway.com>
2024-02-02 13:27:31 -0600

    Add active state to JournalEntry component

--- client/components/app/journal-entry.js
+++ client/components/app/journal-entry.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 
-export default function JournalEntry({ id, tag, date, entryText, symbol, onClick}) {
+export default function JournalEntry({ id, tag, date, entryText, symbol, isActive, onClick}) {
     const [hover, setHover] = useState(false);
 
     function toggleHover() {
@@ -8,17 +8,17 @@ export default function JournalEntry({ id, tag, date, entryText, symbol, onClick
     }
 
     const cursor = onClick ? "cursor-pointer" : "cursor-default";
-    const background = hover ? "bg-green-400" : "bg-transparent";
-
+    const background = (hover || isActive) ? "bg-green-400" : "bg-transparent";
+    // TODO: refactor to tailwind hover: to handle background styling
     return (<div className={`flex flex-col w-full p-1 ${cursor} ${background}`} onClick={() => onClick && onClick(id)} onMouseEnter={toggleHover} onMouseLeave={toggleHover}>
         <div className="flex flex-row justify-between font-bold">
-            <span>{tag}</span>
+            <span>{`#${tag}`}</span>
             {symbol && <span>{symbol}</span>}
         </div>
         <div>
             <span className="text-sm text-gray-500">{date}</span>
         </div>
-        <p className="line-clamp-1 mb-2">
+        <p className="line-clamp-2 mb-2">
             {entryText}
         </p>
     </div>)
