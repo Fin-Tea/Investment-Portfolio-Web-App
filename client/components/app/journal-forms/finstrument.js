411ad46d0ed6b05c3acbef0f08706348ec145a0e
Jabari Holloway <jabari@jabariholloway.com>
2024-02-02 13:30:02 -0600

    Add schema validation, react-hook-form, and wiring to Finstruments form

--- client/components/app/journal-forms/finstrument.js
+++ client/components/app/journal-forms/finstrument.js
@@ -1,52 +1,155 @@
-import React, { useState } from "react";
+import { useForm } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
+import * as Yup from "yup";
 import BaseForm from "./base-form";
 import Pill from "../pill";
 import Autocomplete from "../autocomplete";
 
+const symbolFixtures = [{ label: "ES" }, { label: "NQ" }];
 
-export default function Finstrument({ id, onSubmit }) {
-  const [instrumentTypeId, setinstrumentTypeId] = useState(1);
-  const [symbol, setSymbol] = useState("");
-  const [observations, setObservations] = useState("");
+export default function Finstrument({ data, items, onSubmit, onDelete }) {
+    const { securitySymbols } = items;
+  const validationSchema = Yup.object().shape({
+    securityTypeId: Yup.number()
+      .transform((value) => (Number.isNaN(value) ? null : value))
+      .required("Type required"),
+    securitySymbol: Yup.object().required("Symbol is required"),
+    observations: Yup.string().required("Observations are required"),
+  });
 
+  const formOptions = {
+    defaultValues: {
+      securityTypeId: data?.finstrument.securityTypeId,
+      securitySymbol: securitySymbols.find(
+        ({ label }) => label === data?.finstrument.securitySymbol
+      ),
+      observations: data?.finstrument.observations,
+    },
+    resolver: yupResolver(validationSchema),
+  };
 
-  function handlePillClick({id}) {
-    setinstrumentTypeId(id);
+  const { register, handleSubmit, formState, setValue, watch } =
+    useForm(formOptions);
+  const { errors } = formState;
+
+  const securityTypeId = watch("securityTypeId");
+  const securitySymbol = watch("securitySymbol");
+
+  function handlePillClick({ id }) {
+    setValue("securityTypeId", id);
   }
 
   return (
-    <BaseForm header="Finstrument">
+    <BaseForm
+      header="Finstrument"
+      edit={!!data}
+      onSave={handleSubmit(onSubmit)}
+      onDelete={onDelete}
+    >
       <div>
         <label className="text-sm ml">Type*</label>
         <div className="flex flex-wrap">
-            <Pill className="mr-2 mb-2" key={1} id={1} controlled onClick={handlePillClick} isActive={instrumentTypeId === 1} >Stock</Pill>
-            <Pill className="mr-2 mb-2" key={2} id={2} controlled onClick={handlePillClick} isActive={instrumentTypeId === 2} >ETF</Pill>
-            <Pill className="mr-2 mb-2" key={3} id={3} controlled onClick={handlePillClick} isActive={instrumentTypeId === 3} >Bond</Pill>
-            <Pill className="mr-2 mb-2" key={4} id={4} controlled onClick={handlePillClick} isActive={instrumentTypeId === 4} >Options</Pill>
-            <Pill className="mr-2 mb-2" key={5} id={5} controlled onClick={handlePillClick} isActive={instrumentTypeId === 5} >Futures</Pill>
-            <Pill className="mr-2 mb-2" key={6} id={6} controlled onClick={handlePillClick} isActive={instrumentTypeId === 6} >Crypto</Pill>
-            <Pill className="mr-2 mb-2" key={7} id={7} controlled onClick={handlePillClick} isActive={instrumentTypeId === 7} >Forex</Pill>
+          {/* TODO: fetch instrument types from api */}
+          <Pill
+            className="mr-2 mb-2"
+            key={1}
+            id={1}
+            controlled
+            onClick={handlePillClick}
+            isActive={securityTypeId === 1}
+          >
+            Stock
+          </Pill>
+          <Pill
+            className="mr-2 mb-2"
+            key={2}
+            id={2}
+            controlled
+            onClick={handlePillClick}
+            isActive={securityTypeId === 2}
+          >
+            ETF
+          </Pill>
+          <Pill
+            className="mr-2 mb-2"
+            key={3}
+            id={3}
+            controlled
+            onClick={handlePillClick}
+            isActive={securityTypeId === 3}
+          >
+            Bond
+          </Pill>
+          <Pill
+            className="mr-2 mb-2"
+            key={4}
+            id={4}
+            controlled
+            onClick={handlePillClick}
+            isActive={securityTypeId === 4}
+          >
+            Options
+          </Pill>
+          <Pill
+            className="mr-2 mb-2"
+            key={5}
+            id={5}
+            controlled
+            onClick={handlePillClick}
+            isActive={securityTypeId === 5}
+          >
+            Futures
+          </Pill>
+          <Pill
+            className="mr-2 mb-2"
+            key={6}
+            id={6}
+            controlled
+            onClick={handlePillClick}
+            isActive={securityTypeId === 6}
+          >
+            Crypto
+          </Pill>
+          <Pill
+            className="mr-2 mb-2"
+            key={7}
+            id={7}
+            controlled
+            onClick={handlePillClick}
+            isActive={securityTypeId === 7}
+          >
+            Forex
+          </Pill>
+        </div>
+        <div className="text-red-600 text-xs">
+          {errors.securityTypeId?.message}
         </div>
       </div>
       <div className="mt-4">
         <label className="text-sm ml">Symbol*</label>
         <Autocomplete
-          items={[{ label: "ES" }, { label: "NQ" }]}
-          onSearch={(value) => setSymbol(value)}
-          onSelect={({ label }) => setSymbol(label)}
+          items={securitySymbols}
+          onSelect={(val) => setValue("securitySymbol", val)}
           tooltip="The symbol of the financial instrument being invested in or traded (e.g. AAPL for Apple)"
-          value={symbol}
+          value={securitySymbol}
         />
+        <div className="text-red-600 text-xs">
+          {errors.securitySymbol?.message}
+        </div>
       </div>
-
 
       <div className="mt-4">
         <label className="text-sm ml">Observations*</label>
         <div className="flex items-top">
-          <textarea className="border w-full rounded-md p-2" value={observations} onChange={(e) => setObservations(e.target.value)} />
+          <textarea
+            className="border w-full rounded-md p-2"
+            {...register("observations")}
+          />
+        </div>
+        <div className="text-red-600 text-xs">
+          {errors.observations?.message}
         </div>
       </div>
-
     </BaseForm>
   );
 }
