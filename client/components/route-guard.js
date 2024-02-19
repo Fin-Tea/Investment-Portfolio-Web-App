20ca886296a53cbbea774e08629126119dc0a030
Jabari Holloway <jabari@jabariholloway.com>
2024-02-19 12:50:07 -0600

    Fix authorization render bug

--- client/components/route-guard.js
+++ client/components/route-guard.js
@@ -1,16 +1,21 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useRouter } from "next/router";
 import useAuth from "../hooks/auth";
+import Loader from "./loader";
 
 export default function RouteGuard({ children }) {
   const router = useRouter();
+  const [authorized, setAuthorized] = useState(router.pathname === "/login");
   const { getCachedUser } = useAuth();
 
   useEffect(() => {
-    if (router.pathname !== "/login" && !getCachedUser()) {
+    const isAuthorized = !!getCachedUser() || router.pathname === "/login";
+    if (!isAuthorized) {
       router.push("/login");
+    } else {
+    setAuthorized(isAuthorized);
     }
-  }, []);
+  }, [router.pathname]);
 
-  return <div>{children}</div>;
+  return <div>{ authorized ? children : <Loader color="rgb(107,33,168)" />}</div>;
 }
