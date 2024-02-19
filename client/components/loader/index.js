989d40e9c972490ada89aa80555801d8ba8b9b2b
Jabari Holloway <jabari@jabariholloway.com>
2024-02-19 12:39:31 -0600

    Add color prop to Loader component

--- client/components/loader/index.js
+++ client/components/loader/index.js
@@ -1,10 +1,10 @@
 import { Audio } from "react-loader-spinner";
 import styles from "./loader.module.css";
 
-export default function Loader() {
+export default function Loader({ color }) {
   return (
     <div className={styles.loaderContainer}>
-      <Audio heigth="100" width="100" color="grey" arialLabel="loading" />
+      <Audio heigth="100" width="100" color={color || "grey"} arialLabel="loading" />
     </div>
   );
 }
