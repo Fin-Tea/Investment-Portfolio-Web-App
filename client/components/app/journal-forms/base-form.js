f107328a47ddc83d41eb0039e8fa033e7644135c
Jabari Holloway <jabari@jabariholloway.com>
2024-02-19 18:27:21 -0600

    Add date to BaseForm header

--- client/components/app/journal-forms/base-form.js
+++ client/components/app/journal-forms/base-form.js
@@ -1,13 +1,15 @@
 import React, {useState} from "react";
 import DeleteConfirmationModal from "../delete-confirmation-modal";
+import { formatJournalDate } from "../../../date-utils";
 
-export default function BaseForm({ edit, disabled, header, onSave, onDelete, children }) {
+export default function BaseForm({ edit, disabled, header, date, onSave, onDelete, children }) {
     const [showDeleteModal, setShowDeleteModal] = useState(false);
   return (
     <div>
     <div className="w-2/3 border h-full flex flex-col mx-auto rounded-md">
       <div className="text-center">
         <h4 className="text-lg mt-2">{`${!disabled ? edit ? "Edit" : "New" : ""} ${header}`}</h4>
+        {date && <p className="text-sm -mt-2">{formatJournalDate(date)}</p>}
       </div>
       {!disabled && (<div className="flex justify-center px-4">
         <button className="mr-12 rounded-md bg-purple-800 text-white px-4" onClick={() => setShowDeleteModal(true)}>Delete</button>
