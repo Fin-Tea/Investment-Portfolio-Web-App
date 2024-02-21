bd65d5c5f8d54776c4aa193ae04250d934039349
Jabari Holloway <jabari@jabariholloway.com>
2024-02-20 22:44:37 -0600

    Add magicLink flow to auth hook

--- client/hooks/auth.js
+++ client/hooks/auth.js
@@ -6,12 +6,15 @@ export const ERROR_NOT_LOGGED_IN = "Not logged in";
 
 export default function useAuth() {
   const router = useRouter();
+  const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
   const [user, setUser] = useState(null);
+  const [magicLinkSent, setMagicLinkSent] = useState(false);
   const { makeApiCall } = useApi();
 
   async function login({ email, password }) {
     // TODO: maybe refactor this. wanted to make props make sense but it's not DRY
+    setLoading(true);
 
     const resp = await makeApiCall("account/login", "POST", {
       email,
@@ -30,6 +33,7 @@ export default function useAuth() {
     if (apiError) {
       console.log(apiError);
       setError(apiError);
+      setLoading(false);
     } else {
       setCachedUser({
         id,
@@ -43,10 +47,67 @@ export default function useAuth() {
     }
   }
 
+  async function requestMagicLink({ email }) {
+    setLoading(true);
+
+    const resp = await makeApiCall("account/magicLink", "POST", {
+      email,
+    });
+
+    const {
+      error: apiError,
+      emailSent
+    } = resp;
+
+    if (apiError) {
+      console.log(apiError);
+      setError(apiError);
+     
+    } else {
+      setMagicLinkSent(emailSent);
+    }
+    setLoading(false);
+  }
+  
+    async function magicLogin(token) {
+      setLoading(true);
+      const resp = await makeApiCall("account/magicLogin", "POST", {
+        token
+      });
+  
+      const {
+        error: apiError,
+        id,
+        firstName,
+        lastName,
+        accountName,
+        accountLinks,
+      } = resp;
+  
+      if (apiError) {
+        console.log(apiError);
+        setError(apiError);
+        setLoading(false);
+      } else {
+        setCachedUser({
+          id,
+          firstName,
+          lastName,
+          accountName,
+          accountLinks,
+          selectedAccountId: id,
+        });
+        router.push("/");
+      }
+    }
+
+
   async function logout() {
+    setLoading(true);
     // TODO: keep a log of logins and logouts
     await makeApiCall("account/logout", "POST");
     setCachedUser(null);
+    setLoading(false);
     router.push("/login");
   }
 
@@ -79,12 +140,19 @@ export default function useAuth() {
 
   useEffect(() => {
     setUser(getCachedUser());
-  }, []);
+    if (router.isReady && !router.query.token) {
+      setLoading(false);
+    }
+  }, [router.isReady]);
 
   // need to set accountInfo in-memory at app/layout level so i have the id for api calls
   return {
+    loading,
     error,
     login,
+    requestMagicLink,
+    magicLinkSent,
+    magicLogin,
     logout,
     user,
     getCachedUser,
