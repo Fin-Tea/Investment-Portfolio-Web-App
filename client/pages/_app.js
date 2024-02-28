8f172f6d313c3d99ef2e423fed90b09d1f0750d2
Jabari Holloway <jabari@jabariholloway.com>
2024-02-28 15:25:01 -0600

    Add Hot Jar

--- client/pages/_app.js
+++ client/pages/_app.js
@@ -10,8 +10,9 @@ function MyApp({ Component, pageProps }) {
   return (
     <div>
       {process.env.NODE_ENV === "production" && (
-        <Script id="google-tag-manager" strategy="afterInteractive">
-          {`
+        <div>
+          <Script id="google-tag-manager" strategy="afterInteractive">
+            {`
 <!-- Google Tag Manager -->
 <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
 new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
@@ -20,7 +21,20 @@ j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
 })(window,document,'script','dataLayer','${process.env.NEXT_PUBLIC_GTM}');</script>
 <!-- End Google Tag Manager -->
       `}
-        </Script>
+          </Script>
+          <Script id="google-tag-manager" strategy="afterInteractive">
+            {`
+    (function(h,o,t,j,a,r){
+      h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+      h._hjSettings={hjid:${process.env.NEXT_PUBLIC_HOT_JAR},hjsv:6};
+      a=o.getElementsByTagName('head')[0];
+      r=o.createElement('script');r.async=1;
+      r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+      a.appendChild(r);
+  })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
+      `}
+          </Script>
+        </div>
       )}
       <RouteGuard>
         <ChakraProvider>
