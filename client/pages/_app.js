722b81fdd579566e1899901f2df35f26f1ada47d
Jabari Holloway <jabari@jabariholloway.com>
2024-02-28 15:36:45 -0600

    Fix Hot Jar script tag

--- client/pages/_app.js
+++ client/pages/_app.js
@@ -24,14 +24,14 @@ j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
           </Script>
           <Script id="google-tag-manager" strategy="afterInteractive">
             {`
-    (function(h,o,t,j,a,r){
+    <script>(function(h,o,t,j,a,r){
       h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
       h._hjSettings={hjid:${process.env.NEXT_PUBLIC_HOT_JAR},hjsv:6};
       a=o.getElementsByTagName('head')[0];
       r=o.createElement('script');r.async=1;
       r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
       a.appendChild(r);
-  })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
+  })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');</script>
       `}
           </Script>
         </div>
