bea6829379bb9704553eef7700bafbfe72d14768
Jabari Holloway <jabari@jabariholloway.com>
2024-02-28 15:56:06 -0600

    Remove extra script tags

--- client/pages/_app.js
+++ client/pages/_app.js
@@ -14,24 +14,24 @@ function MyApp({ Component, pageProps }) {
           <Script id="google-tag-manager" strategy="afterInteractive">
             {`
 <!-- Google Tag Manager -->
-<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
 new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
 j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
 'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','${process.env.NEXT_PUBLIC_GTM}');</script>
+})(window,document,'script','dataLayer','${process.env.NEXT_PUBLIC_GTM}');
 <!-- End Google Tag Manager -->
       `}
           </Script>
           <Script id="hotjar-tag" strategy="afterInteractive">
             {`
-    <script>(function(h,o,t,j,a,r){
+    (function(h,o,t,j,a,r){
       h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
       h._hjSettings={hjid:${process.env.NEXT_PUBLIC_HOT_JAR},hjsv:6};
       a=o.getElementsByTagName('head')[0];
       r=o.createElement('script');r.async=1;
       r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
       a.appendChild(r);
-  })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');</script>
+  })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
       `}
           </Script>
         </div>
